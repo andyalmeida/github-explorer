295abd2f1b2237552e031b33b8a723eacdd42825
André Almeida <andalvalmeida@gmail.com>
2020-05-05 23:42:01 -0300

    :lipstick: Create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  #root {
+    max-width: 960px;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: 40px 20px;
+    background: url(${githubBackground}) no-repeat right top;
+  }
+`;
